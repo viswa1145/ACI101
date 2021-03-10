d85c252df146f8df9de6715054a74dae00f8b536
viswanatha <viswa1145@users.noreply.github.com>
2021-03-10 16:01:59 +0530

    Update AppDelegate.swift

--- Start/Minutes/Minutes/AppDelegate.swift
+++ Start/Minutes/Minutes/AppDelegate.swift
@@ -1,5 +1,11 @@
 import UIKit
 
+import AppCenter
+import AppCenterDistribute
+import AppCenterCrashes
+import AppCenterAnalytics
+import AppCenterPush
+
 //9d61d868-63c7-43b6-8988-f2f33425c65e
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate
@@ -10,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
+        MSAppCenter.start("9d61d868-63c7-43b6-8988-f2f33425c65e", withServices: [MSDistribute.self, MSCrashes.self, MSAnalytics.self, MSPush.self])
         return true
     }
 
